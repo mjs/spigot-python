553d8797fd450b792f32b45c491fcbc3db571d2d
Menno Finlay-Smits <hello@menno.io>
2023-08-13 13:59:45 +1200

    Tidied up block damage

--- python/block-damage.py
+++ python/block-damage.py
@@ -1,9 +1,6 @@
 from mcapi import *
-from time import sleep
-from random import randint
 
 from org.bukkit.event.block import BlockDamageEvent
-from org.bukkit.event.player import PlayerInteractEntityEvent
 
 remove_event_listeners()
 
@@ -12,12 +9,7 @@ def damage_event(e):
     player = e.getPlayer()
     health = player.getHealth()
     player.setHealth(health - 1)
-    #b = e.getBlock()
+    print("ow!")
 
 listener = add_event_listener(BlockDamageEvent, damage_event)
 
-#@asynchronous()
-#def interact_event(e):
-    #yell("interact")
-
-#listener = add_event_listener(PlayerInteractEntityEvent, interact_event)
